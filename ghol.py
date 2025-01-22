0cd5f54a3ffe1919f20bbada929fbfc583b22e5c
amirali <amiraliarzande8@gmail.com>
2025-01-22 14:44:40 +0330

    add 2 ghol.py

---
+++ ghol.py
@@ -0,0 +1,10 @@
+x=int(input("vorodi 1 :"))
+y=int(input("vorodi 2 :"))
+z=int(input("vorodi 3 :"))
+if x > y and x > z :
+    print("x bozorg")
+elif y > z :
+    print ("y bozorg")
+else:
+    print("z bozorg")
+#def
